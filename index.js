25a8fe0ab8793bf140edc2e233ced32c9ea89f1d
Oscar Bernal <oscarbernal93@gmail.com>
2020-08-22 08:09:31 -0500

    Users list updating properly
    Signed-off-by: Oscar Bernal <oscarbernal93@gmail.com>

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ let app = require('express')();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
+var players = {};
+
 app.get('/',(req,res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+  io.emit('players_info', players);
   socket.on('name', (name) => {
     console.log(name + " is ready to play")
-    io.emit('user_in', name);
+    players[socket.id] = name
+    io.emit('players_info', players);
   });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    delete players[socket.id]
+    io.emit('players_info', players);
+  });
+
 });
 
 http.listen(3000,() => {
