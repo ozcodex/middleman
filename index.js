1a76f577c3a10eabee0d26e37609437e52888fca
Oscar Bernal <oscarbernal93@gmail.com>
2020-08-22 10:34:41 -0500

    Adds offer form after start the game (and fix a bux with save a&b numbers)
    Signed-off-by: Oscar Bernal <oscarbernal93@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-let app = require('express')();
+let express = require('express');
+let app = express() 
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
+// Express Config
+app.use(express.static('public'))
+app.get('/',(req,res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
 var players = {};
 var data = {
-  status : 0
+  status : 0,
+  initial_money: 0
 }
 
 function calculate(){
@@ -13,17 +22,18 @@ function calculate(){
   data['initial_money'] = 10 * n;
 }
 
-app.get('/',(req,res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+
 
 io.on('connection', (socket) => {
   
   io.emit('players_info', players);
   io.emit('data',data)
 
-  socket.on('name', (name) => {
-    players[socket.id] = name
+  socket.on('join_game', (player_data) => {
+    players[socket.id] = {}
+    players[socket.id]['name']= player_data.name
+    players[socket.id]['num_a']= player_data.num_a
+    players[socket.id]['num_b']= player_data.num_b
     io.emit('players_info', players);
   });
 
