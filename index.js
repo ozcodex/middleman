41680bfc288a7592a24ef89a157114751a428bc9
Oscar Bernal <oscarbernal93@gmail.com>
2020-08-22 08:40:53 -0500

    Adds play in progress functionality
    Signed-off-by: Oscar Bernal <oscarbernal93@gmail.com>

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
 var players = {};
+var data = {
+  status : 0
+}
 
 app.get('/',(req,res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+  
   io.emit('players_info', players);
+  io.emit('data',data)
+
   socket.on('name', (name) => {
     console.log(name + " is ready to play")
     players[socket.id] = name
     io.emit('players_info', players);
+  });
+
+  socket.on('start', ()=> {
+    data.status = 1;
+    io.emit('data',data);
   });
 
   socket.on('disconnect', () => {
